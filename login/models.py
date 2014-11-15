ab370e7c5061f3e74f834f63a1a869f3e05e7f3b
himanshu <ho2es@virgina.edu>
2014-11-15 14:57:45 -0500

    init

---
+++ login/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    fb_id = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name+":"+self.fb_id
+
+
+
+class Compliments(models.Model):
+    compliment = models.Textfield()
+    for_user_id = models.ForeignKey(User)    #Foreign Key
+    written_by_id = models.ForeignKey(User)  #Foreign Key
+    def __str__(self):
+        return self.compliment
+
+
+
+
+
+
+
+#a = User()
+
+
+#a.toString()      ======      str(a)
---
+++ login/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+from django.http import HttpResponseRedirect
+
+#invoked when the url pattern is "/"
+def index(request):
+    #the post request occurs when the form is filled and submitted
+    """
+    if request.method == 'POST': #redirect to "/bulletin" for post request
+
+        #request.POST <---list
+        if request.POST is not None:
+
+            request_email = request.POST['email']
+            request_password = request.POST['password']
+
+
+            try:
+                temp = User.objects.filter(login=request_email, password=request_password)
+                if len(temp)==1:
+                    request.session['user_id'] = request_email
+
+
+                    return HttpResponseRedirect('/bulletin')
+                else:
+                    return HttpResponse("email or password is incorrect.")
+            except:
+                return HttpResponse("email or password is incorrect.")
+
+    """
+    #the get request occurs when the page is loaded
+    if request.method == "GET":
+        template = loader.get_template('login/index.html') #creates a template object from the html file
+        context = RequestContext(request, {})
+        return HttpResponse(template.render(context)) #return the http response that includes the html page

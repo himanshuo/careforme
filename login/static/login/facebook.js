ec1305dbec3102996c812ceca31cac98349a0b6d
himanshu <ho2es@virgina.edu>
2014-11-15 16:13:38 -0500

    working

--- login/static/login/facebook.js
+++ login/static/login/facebook.js
@@ -4,4 +4,10 @@
 
 
 
-console.log("hi, it should be working.")
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=1578751839011228&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
